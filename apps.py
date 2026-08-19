222d9e7266ea9c05f211d373ab2442b60b2bbbff
Tharun Muthiyalu Jothir <tharun.muthiyalu@gmail.com>
2026-08-19 14:31:29 +0200

    Removed Discord and Steam Opening

--- apps.py
+++ apps.py
@@ -5,7 +5,5 @@ APPS = {
     "calculator": "calc.exe",
     "chrome": r"C:\Program Files\Google\Chrome\Application\chrome.exe",
     "file explorer": "explorer.exe",
-    "discord": r"C:\Users\tharu\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord.lnk",
-    "steam": r"C:\Users\tharu\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Steam.lnk",
     # Add more apps as needed
 }
